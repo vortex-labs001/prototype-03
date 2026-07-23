7632ef9d5cf1f131c5257d2aa75f3b6ecab57a73
vortex-labs001 <gekkoshowdown@gmail.com>
2026-07-23 22:18:21 +0530

    Delete fix.js

--- fix.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const idxPath = path.join(__dirname, '..', '..', 'brain', '19d9bc7c-df88-46ac-b58d-c327dad9dcab', 'index.tsx');
-let content = fs.readFileSync(idxPath, 'utf8');
-content = content.replace(/\/images\//g, 'images/');
-fs.writeFileSync(idxPath, content);
-console.log('Fixed image paths');
